7a909fdb38a5144ee394e67c7624660e0f6dd105
Vitor de Souza <vitordesolza@gmail.com>
2022-04-07 11:07:41 -0300

    Added PipeFunctions

---
+++ Console.PipeFunctions/PipeFunctions.cs
@@ -0,0 +1,9 @@
+﻿namespace PipeFunctions;
+
+public static class PipeFunctions
+{
+    public static TOut? Pipe<TIn, TOut>(this TIn? input, Func<TIn?, TOut?>? func)
+    {
+        return func != null ? func.Invoke(input) : default;
+    }
+}
---
+++ Console.PipeFunctions/Program.cs
@@ -0,0 +1,14 @@
+﻿using PipeFunctions;
+const string name = "Gerimu";
+
+name.Pipe(v =>
+{
+    var hello = $"Hello {v}";
+    Console.WriteLine(hello);
+    return hello;
+})
+    .Pipe(v =>
+    {
+        Console.WriteLine("It's a pleasure to meet you");
+        return v;
+    });
