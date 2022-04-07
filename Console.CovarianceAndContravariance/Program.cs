f15fc428544aae2ddc9a2b12245d2aec0d3dc10c
Vitor de Souza <vitordesolza@gmail.com>
2022-04-07 11:10:51 -0300

    Added CovarianceAndContravariance

---
+++ Console.CovarianceAndContravariance/Program.cs
@@ -0,0 +1,20 @@
+﻿interface IVeihcle { }
+class Car : IVeihcle { }
+class Motocycle : IVeihcle { }
+
+interface ICovariance<out T>
+{
+    T Value { get; }
+}
+
+interface IContravariante<in T>
+{
+    void Set(T value);
+}
+
+partial class Program
+{
+
+    ICovariance<IVeihcle> covariance = (ICovariance<Car>)null;
+    IContravariante<Motocycle> contravariante = (IContravariante<IVeihcle>)null;
+}
