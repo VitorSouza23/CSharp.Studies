ce0f8177d41521dfd4e3e1551429b09e69a0d63d
Vitor de Souza <vitordesolza@gmail.com>
2022-04-22 11:23:07 -0300

    Added Console.HigherOrderFunctions

---
+++ Console.HigherOrderFunctions/Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> repository = new();
+
+var application = (string? text, Func<string, bool> store) =>
+{
+    return text switch
+    {
+        null => false,
+        { Length: < 5 } => false,
+        _ => store(text)
+    };
+};
+
+var infrastructure = (string text) =>
+{
+    Console.WriteLine($"Storing: {text}");
+    return true;
+};
+
+var console = (Func<string, bool> store, Func<string?, Func<string, bool>, bool> application) =>
+{
+    Console.WriteLine("What message you want store?");
+    var text = Console.ReadLine();
+    return application(text, store);
+};
+
+var result = console(infrastructure, application);
+Console.WriteLine($"Stored successfully? {result}");
+
+Console.ReadKey();
