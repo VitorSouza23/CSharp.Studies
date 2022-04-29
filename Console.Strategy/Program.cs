6b3c862ee4519720295313bf41939d1bd258f5b4
Vitor de Souza <vitordesolza@gmail.com>
2022-04-29 16:14:54 -0300

    Added Console.Strategy

---
+++ Console.Strategy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Enter a number:");
+string value = Console.ReadLine() ?? "";
+ProcessInput(value);
+
+
+static void ProcessInput(string input)
+{
+    var iRuleType = typeof(IRule);
+    var types = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(s => s.GetTypes())
+        .Where(p => iRuleType.IsAssignableFrom(p) && !p.IsInterface);
+
+    foreach(var type in types)
+    {
+        IRule? rule = (IRule)Assembly.GetExecutingAssembly().CreateInstance(type.FullName, false);
+
+        if (rule is not null && rule.Verify(input))
+            rule.Execute();
+    }
+}
+
+interface IRule
+{
+    bool Verify(string choice);
+    void Execute();
+}
+
+class NotANumberRule : IRule
+{
+    public void Execute()
+    {
+        Console.WriteLine("Please, a number");
+    }
+
+    public bool Verify(string choice)
+    {
+        return !choice.All(char.IsDigit);
+    }
+}
+
+class IsANumberRule : IRule
+{
+    public void Execute()
+    {
+        Console.WriteLine("Nice number!");
+    }
+
+    public bool Verify(string choice)
+    {
+        return choice.All(char.IsDigit);
+    }
+}
