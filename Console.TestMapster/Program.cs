e38298bf078a1e56c39644f8fd374246ef65d923
Vitor de Souza <vitordesolza@gmail.com>
2022-06-13 11:59:10 -0300

    Added Console.TestMapster

---
+++ Console.TestMapster/Program.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+
+var a = new TestA { Name = "Test", Age = 30 };
+var b = a.Adapt<TestB>();
+Console.WriteLine($"Name: {b.Name} - Age: {b.Age}");
+
+internal class TestA
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+internal class TestB
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
