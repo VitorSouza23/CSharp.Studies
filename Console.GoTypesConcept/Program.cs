ed0cd041134b265b6b4a202b0b50a95325c906ab
Vitor de Souza <vitordesolza@gmail.com>
2022-06-14 12:04:44 -0300

    Added Console.GoTypesConcept

---
+++ Console.GoTypesConcept/Program.cs
@@ -0,0 +1,16 @@
+﻿IPerson p = new Person(1, "Test", new DateOnly(2000, 1, 1));
+
+Console.WriteLine($"{p.Name} is {p.CalcualateAge(DateOnly.FromDateTime(DateTime.Now))} years old");
+
+interface IPerson
+{
+    int Id { get; }
+    string Name { get; }
+    DateOnly BirthDay { get; }
+    int CalcualateAge(DateOnly referenceDate)
+    {
+        return Math.Abs(referenceDate.Year - BirthDay.Year);
+    }
+}
+
+record Person(int Id, string Name, DateOnly BirthDay) : IPerson;
