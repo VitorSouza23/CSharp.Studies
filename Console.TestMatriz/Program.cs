d5adf973181198dd88dbf6bf84cd33193e233b90
Vitor de Souza <vitordesolza@gmail.com>
2022-06-28 10:45:32 -0300

    Added Instanciation types

--- Console.TestMatriz/Program.cs
+++ Console.TestMatriz/Program.cs
@@ -5,6 +5,12 @@ int[,] m = { { 1, 2 }, { 3, 4 } }; //Multidimensional Arrays
 Console.WriteLine(m.Rank);
 Console.WriteLine(m.Length);
 
+Console.WriteLine("===========");
+foreach (int x in m)
+{
+    Console.WriteLine(x);
+}
+
 var printMatriz = (int[,] mt) =>
 {
     Console.WriteLine("===========");
@@ -45,12 +51,7 @@ List<List<int>> jaggedList = new()
     new() {6,7,8,9},
 };
 
-int[][] m2 = new int[jaggedList.Count][];
-
-// or
-
-m2 = jaggedList.Select(x => x.ToArray()).ToArray();
-
+int[][] m2 = jaggedList.Select(x => x.ToArray()).ToArray();
 
 var printJagged = (int[][] jg) =>
 {
@@ -65,3 +66,19 @@ var printJagged = (int[][] jg) =>
 };
 
 printJagged(m2);
+
+//Instanciation types:
+
+int[,] t = new int[3, 4];
+int[,] t1 = { { 1, 2, 3}, { 1, 2, 3} };
+
+int[][] t2 = new int[2][];
+t2[0] = new int[3];
+t2[1] = new int[4];
+
+int[][] t3 =
+{
+    new int[] { 1, 2, 3},
+    new int[] { 1, 2, 3},
+    new int[] { 1, 2, 3},
+};
