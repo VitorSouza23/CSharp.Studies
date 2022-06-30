6b67fce29c49b9366b45f33aa70b34ce547a9ea8
Vitor de Souza <vitordesolza@gmail.com>
2022-06-30 15:43:15 -0300

    Added Console.StringAlignment

---
+++ Console.StringAlignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+var number = 1;
+new List<string>
+{
+    //nomral
+    $"|{number}|",
+    //left-aligned
+    $"|{number, -10}|",
+    //right-aligned
+    $"|{number, 10}|"
+}.ForEach(WriteLine);
