64c32e047dc7322699af999698c33f295a608320
Vitor de Souza <vitordesolza@gmail.com>
2022-06-30 16:07:10 -0300

    Added Console.TestLazy

---
+++ Console.TestLazy/Program.cs
@@ -0,0 +1,37 @@
+﻿Customer c = new();
+
+Console.WriteLine($"Customer ID: {c.Id}");
+Console.WriteLine($"Order description: {c.Order.Description}"); //The object going to initialized here
+
+
+class Customer
+{
+    private readonly Lazy<Order> _orders;
+
+    public Guid Id { get; }
+
+    public Customer()
+    {
+        Id = Guid.NewGuid();
+        _orders = new(() => new Order(Id, "Test")); //The object going to just be initialized when called
+    }
+
+    public Order Order
+    {
+        get => _orders.Value;
+    }
+}
+
+class Order
+{
+    public Order(Guid customerId, string description)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        Description = description;
+    }
+
+    public Guid Id { get; }
+    public Guid CustomerId { get; set; }
+    public string Description { get; set; }
+}
