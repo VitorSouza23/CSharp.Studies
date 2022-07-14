fcd1a77b4d7a58a34d9a2424f6c9362cd475ee36
Vitor de Souza <vitordesolza@gmail.com>
2022-07-14 08:32:26 -0300

    Added FSharpInteroperability

---
+++ Console.FSharpInteroperability/Program.cs
@@ -0,0 +1,10 @@
+﻿using FSharpCalculations;
+
+var vcl = new Attribute(0.987, Calculations.Quality.Good);
+var refDensity = new Attribute(15.0, Calculations.Quality.Uncertain);
+
+var density = new Attribute(Calculations.density(refDensity.Value, vcl.Value), Calculations.quality(refDensity.Quality, vcl.Quality));
+
+Console.WriteLine(density);
+
+internal record Attribute(double Value, Calculations.Quality Quality);
---
+++ FSharpCalculations/Calculations.fs
@@ -0,0 +1,17 @@
+﻿namespace FSharpCalculations
+
+module Calculations = 
+    type Quality = 
+        | Good
+        | Uncertain
+        | Bad
+
+    let density (refDensity: double) (vcl: double) = refDensity * vcl
+
+    let quality quality1 quality2 =
+        match (quality1, quality2) with
+        | (Bad, _) | (_, Bad) -> Bad
+        | (Uncertain, _) | (_, Uncertain) -> Uncertain
+        | (Good, Good) -> Good
+
+
