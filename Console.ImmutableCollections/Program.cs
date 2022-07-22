7bbfacbe2df9b23fcbfbbfa36cf115c0b26ace00
Vitor de Souza <vitordesolza@gmail.com>
2022-07-22 08:39:27 -0300

    Added Console.ImmutableCollections

---
+++ Console.ImmutableCollections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+
+IImmutableList<int> immutableList = ImmutableList.Create<int>();
+
+immutableList = immutableList.Add(0);
+immutableList = immutableList.Add(1);
+immutableList = immutableList.Add(2);
+
+foreach(int x in immutableList)
+    Console.WriteLine(x);
+
+immutableList = immutableList.Remove(0);
+
+foreach (int x in immutableList)
+    Console.WriteLine(x);
+
+
+Customer customer = new() { Name = "Test", Id = Guid.NewGuid()};
+
+IImmutableList<Customer> immutableList2 = ImmutableList.Create<Customer>();
+immutableList2 = immutableList2.Add(customer);
+
+Console.WriteLine(immutableList2[0].Name);
+
+customer.Name = "Changed Name";
+
+Console.WriteLine(immutableList2[0].Name);
+
+
+internal class Customer
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+}
