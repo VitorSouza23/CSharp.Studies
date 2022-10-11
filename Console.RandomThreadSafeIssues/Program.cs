743e1d642a9af9dc99258f618a2e4fe6907c9eb8
Vitor de Souza <vitordesolza@gmail.com>
2022-10-11 11:45:40 -0300

    Added Console.RandomThreadSafeIssues.csproj

---
+++ Console.RandomThreadSafeIssues/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+WriteLine("Case 1: Seed constructor is not Thread Safe");
+ParallelTest(new Random(532));
+
+WriteLine();
+WriteLine("Case 2: Derivative class constructor uses dotnet5 compatibility");
+ParallelTest(new MyRandom());
+
+WriteLine();
+WriteLine("Case 3: After dotnet6 default constructor is Thread Safe");
+ParallelTest(new Random());
+
+WriteLine();
+WriteLine("Case 4: Random.Shared alternative");
+ParallelTest(Random.Shared);
+
+void ParallelTest(Random random)
+{
+    Parallel.For(1, 20, _ =>
+    {
+        var numbers = new int[10_000];
+        for (var i = 0; i < numbers.Length; i++) numbers[i] = random.Next();
+        var issues = numbers.Count(x => x == 0);
+        WriteLine($"Issues: {issues}");
+    });
+}
+
+class MyRandom : Random {}
