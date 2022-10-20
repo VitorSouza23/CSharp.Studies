150e1d019cd3a00fecc8431b9af41e0972e42a07
Vitor de Souza <vitordesolza@gmail.com>
2022-10-20 15:04:40 -0300

    Added Console.TaskCancelationToken

---
+++ Console.TaskCancelationToken/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+var source = new CancellationTokenSource();
+var token = source.Token;
+
+Task.Run(async () =>
+{
+    while (true)
+    {
+        WriteLine("I'm running!");
+        var wait = TimeSpan.FromSeconds(1);
+        await Task.Delay(wait, token);
+    }
+}, token);
+
+ReadKey();
+source.Cancel();
+WriteLine("Canceled!");
+ReadKey();
