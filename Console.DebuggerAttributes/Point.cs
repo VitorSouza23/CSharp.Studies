b0d3efd80f3f625bae52add7ce40aa84bbcbd51a
Vitor de Souza <vitordesolza@gmail.com>
2022-10-24 10:10:39 -0300

    Added Console.DebuggerAttributes

---
+++ Console.DebuggerAttributes/Point.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+
+namespace Console.DebuggerAttributes;
+
+[DebuggerDisplay("X: {X}, Y: {Y}")]
+public class Point
+{
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+
+    public double DistanceTo(Point other)
+    {
+        return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+    }
+}
---
+++ Console.DebuggerAttributes/Program.cs
@@ -0,0 +1,8 @@
+﻿using Console.DebuggerAttributes;
+using static System.Console;
+
+var point1 = new Point(3, 4);
+var point2 = new Point(5, 6);
+
+WriteLine(point1.DistanceTo(point2));
+ReadKey();
