7ec1a532f35ceb4721cd389901cde66ce2a9dda2
Vitor de Souza <vitordesolza@gmail.com>
2022-10-25 14:54:51 -0300

    Added Console.TestTimeSpan

---
+++ Console.TestTimeSpan/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.TimeSpan;
+
+var day = FromDays(1);
+var hour = FromHours(1);
+var minutes = FromMinutes(1);
+var seconds = FromSeconds(1);
+
+Console.WriteLine($"Days: {day}");
+Console.WriteLine($"Hours: {hour}");
+Console.WriteLine($"Minutes: {minutes}");
+Console.WriteLine($"Seconds: {seconds}");
