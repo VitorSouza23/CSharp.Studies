fcc63b8a4618f56832050e26994742577154fb4e
Vitor de Souza <vitordesolza@gmail.com>
2022-10-27 08:30:17 -0300

    Added Console.ConvertImageToBase64

---
+++ Console.ConvertImageToBase64/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+
+var imagePath = "images.jpeg";
+
+byte[] imageArray = await File.ReadAllBytesAsync(imagePath, default);
+string base64 = Convert.ToBase64String(imageArray);
+WriteLine(base64);
