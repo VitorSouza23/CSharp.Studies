0ed03c2a103a7833435e2afc86c6632775501db1
Vitor de Souza <vitordesolza@gmail.com>
2022-11-09 09:52:44 -0300

    Added JSON raw test

--- Console.TestCSharp11/Program.cs
+++ Console.TestCSharp11/Program.cs
@@ -20,3 +20,30 @@ WriteLine(arr is [_, _, 3, _, _, _]);
 WriteLine(arr is [>= 1, 3, _, _, 6]);
 
 if (arr is [.., var lastElement]) WriteLine(lastElement);
+
+string json = $$"""
+{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+""";
+
+WriteLine(json);
