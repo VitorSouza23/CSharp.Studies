79fe33c95534f3db61859783184c57fc333631df
Vitor de Souza <vitordesolza@gmail.com>
2022-12-06 10:15:30 -0300

    Added Console.EternalProperties

---
+++ Console.ExternalProperties/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+ExternalClass test = new()
+{
+    SomeProperty = 123
+};
+
+test.GetProperties().ExtendedProperty = "New Test";
+
+Console.WriteLine($"Base value {test.SomeProperty} - Extended value: {test.GetProperties().ExtendedProperty}");
+
+class ExternalClass
+{
+    public int SomeProperty { get; set; }
+}
+
+class ExternalClassMetadata
+{
+    public string? ExtendedProperty { get; set; }
+}
+
+static class ExternalClassExtensions
+{
+    public static readonly ConditionalWeakTable<ExternalClass, ExternalClassMetadata> Data = new();
+
+    public static ExternalClassMetadata GetProperties(this ExternalClass obj)
+    {
+        return Data.GetOrCreateValue(obj);
+    }
+}
