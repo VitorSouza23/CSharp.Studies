9fe2c2313b45509ad93132306bb43dcdc33ba8d8
Vitor de Souza <vitordesolza@gmail.com>
2022-12-20 08:59:55 -0300

    Added Console.TestGoTo

---
+++ Console.TestGoTo/Program.cs
@@ -0,0 +1,42 @@
+﻿int i = 0;
+
+repeat:
+if (i < 10)
+{
+    i++;
+    goto repeat;
+}
+
+Console.WriteLine(i);
+
+Console.WriteLine(CalculatePrice(CoffeeChoice.Plain));
+Console.WriteLine(CalculatePrice(CoffeeChoice.WithMilk));
+Console.WriteLine(CalculatePrice(CoffeeChoice.WithIceCream));
+
+static decimal CalculatePrice(CoffeeChoice choice)
+{
+    decimal price = 0;
+    switch (choice)
+    {
+        case CoffeeChoice.Plain:
+            price += 10.0m;
+            break;
+
+        case CoffeeChoice.WithMilk:
+            price += 5.0m;
+            goto case CoffeeChoice.Plain;
+
+        case CoffeeChoice.WithIceCream:
+            price += 7.0m;
+            goto case CoffeeChoice.Plain;
+    }
+
+    return price;
+}
+
+enum CoffeeChoice
+{
+    Plain,
+    WithMilk,
+    WithIceCream
+}
